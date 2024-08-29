03cf9381edc432a6e32a51a7ef11a13adeb86942
GKVS <itsergey1001@gmail.com>
2024-08-29 12:52:06 +0500

    feat: add html task

--- gulp/tasks/html.js
+++ gulp/tasks/html.js
@@ -2,10 +2,12 @@ import { src, dest } from 'gulp';
 import plumber from 'gulp-plumber';
 import plumberConfig from '../utils/plumberConfig.js';
 import changed from 'gulp-changed';
+import ssi from 'gulp-ssi';
 
 export default function html() {
 	return src(['./src/html/**/*.html', '!./src/html/blocks/**/*.html'])
 		.pipe(plumber(plumberConfig('HTML')))
 		.pipe(changed('./app'))
+		.pipe(ssi())
 		.pipe(dest('./app'), { base: './app' })
 }
